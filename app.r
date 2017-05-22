9199a6706ab3a31972460bd55125167106e36608
Andrea <acottais@bittle-solutions.com>
2017-05-22 17:08:05 +0200

    suppr vecteur choice

--- app.r
+++ app.r
@@ -1,5 +1,5 @@
-﻿setwd("/srv/shiny-server/shinyapp")
-#setwd("C:/Users/acottais/Documents/Etudes/localRepo/Mariages")
+﻿#setwd("/srv/shiny-server/shinyapp")
+setwd("C:/Users/acottais/Documents/Etudes/localRepo/Mariages")
 
 if(exists("DDEP")==FALSE){
 source(file="global.r")
@@ -32,7 +32,6 @@ includeCSS("www/test.css"),
 				inputId="dep",
 				label="De quel département venez vous ?",
 				choices= c(rep(1:95), "2A","2B"),
-				#c("1","2","3","4","5","6","7","8","9","10","11","12","13","14","15","16","17","18","19","21","22","23","24","25","26","27","28","29","30","31","32","33","34","35","36","37","38","39","40","41","42","43","44","45","46","47","48","49","50","51","52","53","54","55","56","57","58","59","60","61","62","63","64","65","66","67","68","69","70","71","72","73","74","75","76","77","78","79","80","81","82","83","84","85","86","87","88","89","90","91","92","93","94","95","2A","2B"),
 				selected=NULL,
 				multiple=FALSE,
 				selectize=TRUE
